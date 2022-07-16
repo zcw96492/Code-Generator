03181558918705b8edcd8784e982a9e88611bb27
zhouchaowei <zhouchaowei96492@163.com>
2022-07-16 16:03:29 +0800

    2-初始化提交

---
+++ code-generator-controller/src/main/java/com/newcore/code/generator/controller/CodeGeneratorControllerApplication.java
@@ -0,0 +1,17 @@
+package com.newcore.code.generator.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 代码生成器
+ * @author Zhou Chaowei
+ * @date 2022-07-15
+ */
+@SpringBootApplication
+public class CodeGeneratorControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGeneratorControllerApplication.class, args);
+    }
+}
